0834c607deb00006bc7903c0647cc48b6ae4e522
Zeha Irawan <jangkaringbumi@gmail.com>
2019-10-25 16:46:16 +0700

    Add CMS components

--- merlinstore/src/App.js
+++ merlinstore/src/App.js
@@ -1,17 +1,19 @@
-import React, { Fragment,useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Switch,Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Admin from './components/Admin';
+import Cart from './components/Cart';
 import Footer from './components/Footer';
+import Login from './components/Login';
+import ManageProducts from './components/ManageProducts';
 import Navbar from './components/Navbar';
-import ProductList from './components/ProductList'
-import Login from './components/Login'
-import Register from './components/Register'
-import store from './redux/store';
+import ProductList from './components/ProductList';
+import Register from './components/Register';
 import PrivateRoutes from './components/routing/PrivateRoutes';
 import setAuthToken from './components/utils/setAuthToken';
 import { loadUser } from './redux/actions/auth';
-import Cart from './components/Cart';
+import store from './redux/store';
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -27,10 +29,12 @@ const App = () => {
       <Router>
         <Fragment>
           <Switch>
-          <Route exact path="/" component={Register} />
+            <Route exact path="/" component={Register} />
             <PrivateRoutes exact path="/products" component={ProductList} />
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
+            <Route exact path="/admin" component={Admin} />
+            <Route exact path="/manage-products" component={ManageProducts} />
             <PrivateRoutes exact path="/cart" component={Cart} />
             <Navbar />
             <Footer />
@@ -39,6 +43,6 @@ const App = () => {
       </Router>
     </Provider>
   );
-}
+};
 
 export default App;
